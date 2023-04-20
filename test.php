7690a3c35325a1bd012ec1a86e5982a76e52c47e
Mohamed Lamine <boumaza951@gmail.com>
2023-04-20 13:00:59 +0100

    commit

--- test.php
+++ test.php
@@ -6,17 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
+        .clients-pics {
+            width: 90%;
+            margin: auto;
+            text-align: center;
+        }
+
+        .img-row {
+            display: flex;
+            justify-content: space-evenly;
+        }
+
+
         .card {
-            display: inline-block;
             margin: 10px;
             padding: 10px;
-            border: 1px solid #ddd;
-            box-shadow: 0 0 5px #ddd;
-            max-width: 33%;
+            /* box-shadow: 0 0 5px #ddd; */
+            flex-basis: 30%;
+            position: relative;
         }
 
         .card img {
-            max-width: 100%;
+            width: 100%;
             height: auto;
         }
     </style>
@@ -30,22 +41,36 @@
     // Fetch images from database
     $sql = "SELECT * FROM images";
     $result = mysqli_query($conn, $sql);
+    $images = array();
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
-            $image_id = $row['id'];
-            $image_url = $row['url'];
-            $image_name = $row['name'];
-            $image_story = $row['story'];
+            $images[] = $row;
     ?>
-            <div class="card">
-                <img src="<?php echo $image_url; ?>" alt="<?php echo $image_name; ?>">
-                <div class="caption">
-                    <h4>
-                        <?php echo $image_name; ?>
-                    </h4>
-                    <p>
-                        <?php echo $image_story; ?>
-                    </p>
+            <div class='clients-pics'>
+
+
+                <div class="img-row">
+                    <div class='card'>
+                        <img src='<?php echo $images[0]["url"]; ?>' alt='<?php echo $images[0]["name"]; ?>'>
+                        <div class='caption'>
+                            <h4><?php echo $images[0]["name"]; ?></h4>
+                            <p><?php echo $images[0]["story"]; ?></p>
+                        </div>
+                    </div>
+                    <div class='card'>
+                        <img src='<?php echo $images[1]["url"]; ?>' alt='<?php echo $images[1]["name"]; ?>'>
+                        <div class='caption'>
+                            <h4><?php echo $images[1]["name"]; ?></h4>
+                            <p><?php echo $images[1]["story"]; ?></p>
+                        </div>
+                    </div>
+                    <div class='card'>
+                        <img src='<?php echo $images[2]["url"]; ?>' alt='<?php echo $images[2]["name"]; ?>'>
+                        <div class='caption'>
+                            <h4><?php echo $images[2]["name"]; ?></h4>
+                            <p><?php echo $images[2]["story"]; ?></p>
+                        </div>
+                    </div>
                 </div>
             </div>
     <?php
